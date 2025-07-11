0818cbc7e5c4f885710bc6a6af02f5e0ff71f982
Mykhailo Kazymyr <mykhailokazymyr@gmail.com>
2025-07-11 16:47:35 +0200

    messages

--- app/src/main/java/App.java
+++ app/src/main/java/App.java
@@ -8,6 +8,11 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
-        System.out.println("New changes from dev 2");
+        System.out.println("Here is greetings from second dev");
+        System.out.println("Here is greetings from second dev");
+        System.out.println("Here is greetings from second dev");
+        System.out.println("Here is greetings from second dev");
+        System.out.println("Here is greetings from second dev");
     }
+
 }
